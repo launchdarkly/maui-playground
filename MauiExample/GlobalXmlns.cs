2209278557e0a68d735bdde93acba6164bf0c64d
Andrey Belonogov <abelonogov@launchdarkly.com>
2025-11-13 15:07:58 -0800

    iOS Example with binary framework

---
+++ MauiExample/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiExample")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiExample.Pages")]
---
+++ MauiExample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,25 @@
+﻿using Foundation;
+using UIKit;
+
+#if IOS
+using LDSdk;
+#endif
+
+namespace MauiExample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+	{
+		System.Console.WriteLine("FinishedLaunching: LDClient before");
+#if IOS
+		var result = SRClient.startExample();
+		System.Console.WriteLine("FinishedLaunching: LDClient.startExample()", result);
+#endif
+		System.Console.WriteLine("FinishedLaunching: LDClient end");
+		return base.FinishedLaunching(application, launchOptions);
+	}
+}
